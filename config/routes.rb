43bd42891c3b6f8bf30aa21d962a170f676eabe2
Fred <fpaula@abril.com.br>
2015-09-14 10:19:50 -0300

    Adicionando lista de categorias publicas

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   post 'votes', to: 'votes#create'
   get '/' => 'home#index'
   get '/election/:id', to: 'elections#show', as: :election
+  get '/categories', to: 'categories#index', as: :all_categories
+
   get '/embed/election/:id', to: 'widget#show'
   get '/admin', to: 'admin/dashboard#index', as: :dashboard
 end
