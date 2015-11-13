19fdd505c743fb9c5adf514aa527c9a3a3b9b316
Artur <artur.prado@abril.com.br>
2015-11-13 11:02:57 -0200

    Fix skip candidates

--- app/assets/javascripts/modules/election.js
+++ app/assets/javascripts/modules/election.js
@@ -7,7 +7,7 @@ var Election = function(electionId) {
   this.finishedElection = $('[data-finished-election]');
   this.leftPanel = this.root.find('#left');
   this.rightPanel = this.root.find('#right');
-  this.nextButton = this.root.find('[data-next-candidates]');
+  this.nextButton = $('[data-next-candidates]');
   this.votesCounter = this.root.find('[data-votes-counter]');
   this.combinationId = this.root.find('[data-combination]').attr('data-combination');
 
