d7fdcf81fa19d6adc642095b8a2d3057d7715299
Hissao <achigashi@abril.com.br>
2015-11-13 16:02:20 -0200

    Returning licenses

--- app/models/creative_commons.rb
+++ app/models/creative_commons.rb
@@ -1,6 +1,12 @@
 class CreativeCommons
-  def self.license_url(license)
-    cc, by, version = license.split(' ')
-    "http://creativecommons.org/licenses/#{by.downcase}/#{version}/"
+  def self.licenses
+    {
+      "CC BY 2.0" => "https://creativecommons.org/licenses/by/2.0/",
+      "CC BY-SA 2.0" => "http://creativecommons.org/licenses/by-sa/2.0/",
+      "CC BY 3.0" => "http://creativecommons.org/licenses/by/3.0/",
+      "CC BY-SA 3.0" => "http://creativecommons.org/licenses/by-sa/3.0/",
+      "CC BY 4.0" => "http://creativecommons.org/licenses/by/4.0/",
+      "CC BY-SA 4.0" => "http://creativecommons.org/licenses/by-sa/4.0/"
+    }
   end
 end
